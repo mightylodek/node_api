10fcfe10bc10e0dc7edb525f7144c2b36d2b8b06
George D. Brown <georgedbrown@gmail.com>
2023-04-22 20:11:13 -0500

    am I logged in?

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello NODE API");
 });
 
+app.get("/blog", (req, res) => {
+  res.send("Hello blog, how you doing");
+});
+
 app.listen(3000, () => {
   console.log("Node API app is running on port 3000");
 });
