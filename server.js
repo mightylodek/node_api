69820144dafe7d9ca6a35a4799e1e475bb941991
George D. Brown <georgedbrown@gmail.com>
2023-04-22 20:22:17 -0500

    Testing github remote access

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get("/blog", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Node API app is running on port 3000");
+  console.log("Node API app is running on port 3000 ");
 });
