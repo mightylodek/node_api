226af9266d1368b563b8a914c620726ede6abb00
George D. Brown <georgedbrown@gmail.com>
2023-04-25 22:36:02 -0500

    Added Match endpoints

---
+++ models/matchModel.js
@@ -0,0 +1,79 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var MatchSchema = new Schema(
+  {
+    eventFK: {
+      type: String,
+    },
+    eventName: {
+      type: String,
+    },
+    bracketFK: {
+      type: String,
+    },
+    matchNumber: {
+      type: Number,
+    },
+    team1FK: {
+      type: String,
+    },
+    team2FK: {
+      type: String,
+    },
+    locationFK: {
+      type: MatchLocation,
+    },
+    locationName: {
+      type: String,
+    },
+    team1Score: {
+      type: Number,
+    },
+    team2Score: {
+      type: Number,
+    },
+    maxScore: {
+      type: Number,
+    },
+    winByTwo: {
+      type: Boolean,
+    },
+    startTime: {
+      type: Date,
+    },
+    endTime: {
+      type: String,
+    },
+    plannedDuration: {
+      type: String,
+    },
+    winner: {
+      type: String,
+    },
+    wonBy: {
+      type: String,
+    },
+    winnerNextMatch: {
+      type: Number,
+    },
+    winnerNextMatchPosition: {
+      type: Number,
+    },
+    loserNextMatch: {
+      type: Number,
+    },
+    loserNextMatchPosition: {
+      type: Number,
+    },
+    pointDiff: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Match = mongoose.model("Match", MatchSchema);
+
+module.exports = Match;
