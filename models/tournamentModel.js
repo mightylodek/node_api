cf194db4b884ef8581a7bf640fa2a0527a5ec5f3
George D. Brown <georgedbrown@gmail.com>
2023-07-30 20:38:58 -0500

    Added tournamentmodel and tourneyserver to use it

---
+++ models/tournamentModel.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const participantSchema = mongoose.Schema({
+  id: String,
+  wins: Number,
+  losses: Number,
+  pdiff: Number,
+  seed: Number,
+  name: String,
+  eventName: String,
+});
+
+const matchSchema = mongoose.Schema({
+  bracketName: String,
+  bracketRound: String,
+  matchNumber: Number,
+  team1FK: String,
+  team1Name: String,
+  team1PlayerNames: [String],
+  team1PlayerIds: [String],
+  team2FK: String,
+  team2Name: String,
+  team2PlayerNames: [String],
+  team2PlayerIds: [String],
+  locationFK: String,
+  locationName: String,
+  team1Score: Number,
+  team2Score: Number,
+  maxScore: Number,
+  winByTwo: Boolean,
+  startTime: Date,
+  endTime: String,
+  plannedDuration: String,
+  winner: String,
+  wonBy: String,
+  winnerNextMatch: Number,
+  winnerNextMatchPosition: Number,
+  loserNextMatch: Number,
+  loserNextMatchPosition: Number,
+  pointDiff: Number,
+  matchStatus: String,
+  timestamps: true,
+});
+
+const tournamentSchema = new mongoose.Schema({
+  players: {
+    type: Map,
+    of: playerSchema,
+  },
+
+  matches: {
+    type: Map,
+    of: matchSchema,
+  },
+
+  event: String,
+});
+
+const Tournament = mongoose.model("Tournament", tournamentSchema);
+
+module.exports = Tournament;
