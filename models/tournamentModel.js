be9f5dacb524006f5b1751e0d4d88e391708b47e
George D. Brown <georgedbrown@gmail.com>
2023-07-30 20:49:41 -0500

    Corrected timestamps tournamentModel.js

--- models/tournamentModel.js
+++ models/tournamentModel.js
@@ -1,60 +1,68 @@
 const mongoose = require("mongoose");
 
-const participantSchema = mongoose.Schema({
-  id: String,
-  wins: Number,
-  losses: Number,
-  pdiff: Number,
-  seed: Number,
-  name: String,
-  eventName: String,
-});
-
-const matchSchema = mongoose.Schema({
-  bracketName: String,
-  bracketRound: String,
-  matchNumber: Number,
-  team1FK: String,
-  team1Name: String,
-  team1PlayerNames: [String],
-  team1PlayerIds: [String],
-  team2FK: String,
-  team2Name: String,
-  team2PlayerNames: [String],
-  team2PlayerIds: [String],
-  locationFK: String,
-  locationName: String,
-  team1Score: Number,
-  team2Score: Number,
-  maxScore: Number,
-  winByTwo: Boolean,
-  startTime: Date,
-  endTime: String,
-  plannedDuration: String,
-  winner: String,
-  wonBy: String,
-  winnerNextMatch: Number,
-  winnerNextMatchPosition: Number,
-  loserNextMatch: Number,
-  loserNextMatchPosition: Number,
-  pointDiff: Number,
-  matchStatus: String,
-  timestamps: true,
-});
-
-const tournamentSchema = new mongoose.Schema({
-  players: {
-    type: Map,
-    of: playerSchema,
+const participantSchema = mongoose.Schema(
+  {
+    id: String,
+    wins: Number,
+    losses: Number,
+    pdiff: Number,
+    seed: Number,
+    name: String,
+    eventName: String,
   },
+  { timestamps: true }
+); // Add timestamps option here
 
-  matches: {
-    type: Map,
-    of: matchSchema,
+const matchSchema = mongoose.Schema(
+  {
+    bracketName: String,
+    bracketRound: String,
+    matchNumber: Number,
+    team1FK: String,
+    team1Name: String,
+    team1PlayerNames: [String],
+    team1PlayerIds: [String],
+    team2FK: String,
+    team2Name: String,
+    team2PlayerNames: [String],
+    team2PlayerIds: [String],
+    locationFK: String,
+    locationName: String,
+    team1Score: Number,
+    team2Score: Number,
+    maxScore: Number,
+    winByTwo: Boolean,
+    startTime: Date,
+    endTime: String,
+    plannedDuration: String,
+    winner: String,
+    wonBy: String,
+    winnerNextMatch: Number,
+    winnerNextMatchPosition: Number,
+    loserNextMatch: Number,
+    loserNextMatchPosition: Number,
+    pointDiff: Number,
+    matchStatus: String,
   },
+  { timestamps: true }
+); // Add timestamps option here
 
-  event: String,
-});
+const tournamentSchema = new mongoose.Schema(
+  {
+    players: {
+      type: Map,
+      of: playerSchema,
+    },
+
+    matches: {
+      type: Map,
+      of: matchSchema,
+    },
+
+    event: String,
+  },
+  { timestamps: true }
+); // Add timestamps option here
 
 const Tournament = mongoose.model("Tournament", tournamentSchema);
 
